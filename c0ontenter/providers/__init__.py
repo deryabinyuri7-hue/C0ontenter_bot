2bdee2ca8ce1a7fe52d3cafdee0aef5e8590ada4
deryabinyuri7-hue <deryabinyuri7@gmail.com>
2026-08-13 12:10:35 +0400

    Create __init__.py

---
+++ c0ontenter/providers/__init__.py
@@ -0,0 +1,4 @@
+from c0ontenter.providers.base import GenerationProvider, ProviderTask
+from c0ontenter.providers.kie import KieGenerationProvider
+
+__all__ = ["GenerationProvider", "KieGenerationProvider", "ProviderTask"]
